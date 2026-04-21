d5fb1a0ce8059169bf7b848621b7c1f3669e5163
Ali <alisid@umich.edu>
2026-04-20 22:43:17 -0400

    fixed buffs/effects from duplicating

--- Entity/Components/effectBASE.gd
+++ Entity/Components/effectBASE.gd
@@ -11,13 +11,15 @@ class_name EffectBASE extends GPUParticles2D ## BASE class for effects, effects 
 @export var strength : int = 1          # Effects of a higher strength override lower strengths (& adds time)
 @export var length   : float = 4.00     # How long does the effect last?
 @export var element  : String = ""      # Element
-@export var field    : bool = false     # Is this a field effect? (Timer starts when leave field)
+@export var field    : Node = null     # Reference to the field node
 
 func _ready(): ## Setup the timer, send a status text, particles (if any), and apply any initial effect
 	$Timer.timeout.connect(Destruct.bind(false,true)) # timeout -> Destruct(false, true). This works for field effects too
 	
 	## NOTE: Currently unimplemented
-	#if (field): $Timer.stop() # effect timer is autostart, field timers however start on destructor call
+	if (field): 
+		$Timer.stop() # effect timer is autostart, field timers however start on destructor call
+		print(efname + " field effect added, timer stopped. Timer time_left: " + str($Timer.time_left))
 	
 	if(entity.find_child("Status")): entity.find_child("Status").addStatusText(efname, color)
 	
@@ -29,14 +31,16 @@ func _ready(): ## Setup the timer, send a status text, particles (if any), and a
 	
 	InitialEffect()
 
-func Destruct(skipEndEffect : bool = false, Timeout : bool = false) -> void: ## NOT "_exit_tree()" because this has a param & reparents itself before freeing
+func Destruct(skipEndEffect : bool = false, Timeout : bool = false) -> void: ## NOT "_exit_tree()" because this has a param & reparents itself before freeing\
+	print(efname + " Destruct called | field: " + str(field) + " | Timeout: " + str(Timeout) + " | time_left: " + str($Timer.time_left))
 	if !is_inside_tree(): queue_free(); return # free effect if it isnt even in the tree (e.g. effects stored in fields) # BUG: stored effects are being added to tree
 	
 	## NOTE: Currently unimplemented
-	#if (field && length && !Timeout): $Timer.start(length); return # removing field effects first starts timer, only on timeout -> free
-	
+	if(is_instance_valid(field) && !Timeout): # removing field effects first starts timer, only on timeout -> free
+		$Timer.start(length)
+		field = null
+		return
 	if !skipEndEffect: EndEffect()
-	
 	Tools.ParticlePassOff(self) # Safely reparent particles to let them expire
 
 ## OVERRIDE FUNCS: Inherited effects will override these and supply the functions they wish to perform
