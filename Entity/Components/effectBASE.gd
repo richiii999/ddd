3b809fce06e70efa6b0f41c7d9b61ecd3c8632b2
richiii999 <richiii@umich.edu>
2026-04-23 23:12:16 -0400

    effects TODO

--- Entity/Components/effectBASE.gd
+++ Entity/Components/effectBASE.gd
@@ -1,43 +1,46 @@
-class_name EffectBASE extends GPUParticles2D ## BASE class for effects, effects should inherit from this
-# Contains the effect params, as well as a virtual fn for InitialEffect(), EffectTick(), and EndEffect()
+class_name EffectBASE extends GPUParticles2D ## BASE class for effects, effects inherit this
+# Contains the effect params, as well as virtual fn for Initial(), Tick(), and End()
 
-@onready var entity : Node = Tools.FindParentByType(self, ENTITY) # Which Entity to apply effects to?
+@onready var entity : Node = Tools.FindParentByType(self, ENTITY) # Which Entity to apply effects?
 
-@export var icon     : Resource = null  # TODO: What is the effect's icon? (displays in the top right)
+@export var icon     : Resource = null  # What is the effect's icon?
 @export var color    : String = "WHITE" # Color of status text / entity color change (as a string)
 
-@export var efname   : String = "BASE"  # Reapplications of the same effect adds to timer ("efname" instead of "name" cause reserved word)
-@export var tier     : int = 1          # Effects of a higher tier override lower tiers (resets time)
-@export var strength : int = 1          # Effects of a higher strength override lower strengths (& adds time)
+# NOTE: "efname" since "name" is a reserved word
+@export var efname   : String = "BASE"  # Reapplications of the same effect adds to timer 
+@export var tier     : int = 1          # Effects of a higher tier override lower tiers
+
 @export var length   : float = 4.00     # How long does the effect last?
-@export var field    : Node = null     # Reference to the field node
+@export var field    : Node = null      # (optional) reference to the field node
 
 var lingering : bool = false 
 
-func _ready(): ## Setup the timer, send a status text, particles (if any), and apply any initial effect
-	$Timer.timeout.connect(Destruct.bind(false,true)) # timeout -> Destruct(false, true). This works for field effects too
+func _ready(): ## Setup the timer, send a status text, particles (if any), and apply initial effect
+	$Timer.timeout.connect(Destruct.bind(false,true))
 	
 	if (field): 
 		$Timer.stop() # effect timer is autostart, field timers however start on destructor call
-		#print(efname + " field effect added, timer stopped. Timer time_left: " + str($Timer.time_left))
+		#print(efname + " field effect added, timer stopped. time_left: " + str($Timer.time_left))
 	
 	if(entity.find_child("Status")): entity.find_child("Status").addStatusText(efname, color)
 	
+	## Set the particle effect amount and radius by size of the entity
 	var HitboxSize = entity.find_child("Projectile_Hitbox").get_child(0).shape.size.x
-	if HitboxSize: # Set the particle effect amount and radius (by size of the entity)
+	if HitboxSize:
 		amount = (int)(HitboxSize / 10)
 		process_material.set_emission_ring_radius(HitboxSize * 0.5)
 		process_material.set_emission_ring_inner_radius(HitboxSize * 0.25)
 	
 	InitialEffect()
 
-func Destruct(skipEndEffect : bool = false, Timeout : bool = false) -> void: ## NOT "_exit_tree()" because this has a param & reparents itself before freeing\
+## NOT "_exit_tree()" because this has a param & reparents itself before freeing
+func Destruct(skipEndEffect : bool = false, Timeout : bool = false) -> void: 
 	#print(efname + " Destruct called | field: " + str(field) + " | Timeout: " + str(Timeout) + " | time_left: " + str($Timer.time_left) + " | stack: " + str(get_stack()))
 	if !is_inside_tree(): 
 		queue_free()
-		return # free effect if it isnt even in the tree (e.g. effects stored in fields) # BUG: stored effects are being added to tree
+		# BUG: stored effects are being added to tree
+		return # free effect if it isnt even in the tree (e.g. effects stored in fields) 
 	
-	## NOTE: Currently unimplemented
 	if(field != null && !Timeout): # removing field effects first starts timer, only on timeout -> free
 		lingering = true
 		$Timer.one_shot = true
