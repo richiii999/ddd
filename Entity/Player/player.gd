bf5bf3c9d2c4982e9058be214ef8c63cdd36f759
richiii999 <richiii@umich.edu>
2026-04-29 15:23:47 -0400

    player UI stuff

--- Entity/Player/player.gd
+++ Entity/Player/player.gd
@@ -150,6 +150,11 @@ func get_input(): # TODO: replace this with _input() ?
 	if Input.is_action_just_pressed("X+ (Debug)"): GainXP(9999) # '9'        XP plus for leveling up for debugging
 	if Input.is_action_just_pressed("DEBUG_Bubble"): toggleBubble(!invulnerable) # "Shift+7" Toggles bubble invulnerable
 	
+	# NOTE: EscMenu has to go before the inputStatus check otherwise u get stuck in the menu
+	if Input.is_action_just_pressed("Esc"): 
+		%EscMenu.visible = !(%EscMenu.visible) 
+		InputStatus = !(%EscMenu.visible)
+	
 	if !InputStatus: return # Disallow input if it's disabled
 	
 	## Movement keys:
@@ -158,22 +163,15 @@ func get_input(): # TODO: replace this with _input() ?
 		#get the speed and then cap it as you keep leveling up
 		var spd_mult := get_move_spd()
 		velocity += InputV * (accel * effectMoveSpeed * tileSpeed * spd_mult) + tilePush
-		
 	
 	## Change sprite frame and flip direction based on direction / velocity for player
 	# The "velocity.length() > 10.0" tells us "are we moving fast enough to count as moving?", and the else gives us a vector pointing from the player toward the mouse 
 	var facingDir : Vector2 = velocity if velocity.length() > 10.0 else (get_global_mouse_position() - global_position)
 	# Flip the sprite horizontally when facing the left 
-	if facingDir.x != 0:
-		$AnimatedSprite2D.flip_h = facingDir.x > 0
-		#insert something like $Sprite2D.flip_h = facingDir < 0 
+	if facingDir.x != 0: $AnimatedSprite2D.flip_h = facingDir.x > 0
 	
-	if velocity.length() > 10.0:
-		$AnimatedSprite2D.play("walk")
-		#insert something like $AnimatedSprite2D.play("walk")
-	else:
-		$AnimatedSprite2D.play("idle")
-		#insert something like $AnimatedSprite2D.play("idle")
+	if velocity.length() > 10.0: $AnimatedSprite2D.play("walk")
+	else: $AnimatedSprite2D.play("idle")
 
 	## Mouse inputs: "pressed" NOT "just_pressed" so player can hold shoot / dash
 	# Left Click: Shoot1
@@ -244,8 +242,6 @@ func get_input(): # TODO: replace this with _input() ?
 	## UI Toggles 
 	if Input.is_action_just_pressed("Loading Screen Toggle"):
 		%LoadingScreen.visible = !(%LoadingScreen.visible)
-		#get_tree().set_pause( !(get_tree().is_paused()) ) # Toggle pause
-	if Input.is_action_just_pressed("Esc"): %EscMenu.visible = !(%EscMenu.visible) # Esc manu goes above loading screen, both disable all lower controls and UI interactions.
 	if Input.is_action_just_pressed("F1"): # Controls toggle 'F1'
 		%ControlsText.visible = !(%ControlsText.visible)
 		%HiddenControlsText.visible = !(%HiddenControlsText.visible)
