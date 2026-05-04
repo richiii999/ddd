2248b8ac5e0d534dc9df456e66f0b4fd416b49bd
jharwick23 <jharwick12@gmail.com>
2026-05-04 13:23:50 -0400

    Finished the slime boss

--- Entity/Components/Behaviors/Boss/behavior_crash.gd
+++ Entity/Components/Behaviors/Boss/behavior_crash.gd
@@ -1,10 +1,13 @@
 extends BehaviorBASE ## Crash: When boss crashes into wall
 
+@onready var animated_sprite_2d = $"../../AnimatedSprite2D"
+
 ## Override funcs: behaviorBASE funcs overridden by behavior_crash
 func onEnter(): # Adjust movement for the Entity and begin orbiting
 	Entity.immovable = true
 	Entity.velocity = Vector2.ZERO
 	Entity.set_collision_layer_value(9, false)
+	animated_sprite_2d.pause()
 	
 	#TODO: Enemy should shoot multiple waves of projectiles
 	Entity.EnemyShoot(1)
@@ -16,6 +19,7 @@ func onLeave(): # Stop timer, reset movement stuff
 	Entity.behaviorMoveSpeed = 1.00
 	Entity.targetPosStopRadius = 10.00
 	Entity.set_collision_layer_value(9, true)
+	animated_sprite_2d.play()
 
 func BehaviorTick(): 
 	pass
--- Entity/Enemy/Bosses/slime_boss.gd
+++ Entity/Enemy/Bosses/slime_boss.gd
@@ -9,13 +9,14 @@ class_name SlimeBoss extends Enemy
 # Overrides abstract EnemyShoot function
 func EnemyShoot(P: int, pos: Vector2 = targetEntity.global_position):
 	var dir_to_player = global_position.angle_to_point(pos)
-	pattern.Emit(ProjectilePattern.PatternType.WING_RINGS, 8, {
-		"power": P,
-		"ring_count": 3,
-		"sine_amp": 0.5,
-		"sine_freq": 1.5,
-		"time_offset": Time.get_ticks_msec() / 1000.0
-	})
+	#pattern.Emit(ProjectilePattern.PatternType.WING_RINGS, 8, {
+	#	"power": P,
+	#	"ring_count": 3,
+	#	"sine_amp": 0.5,
+	#	"sine_freq": 1.5,
+	#	"time_offset": Time.get_ticks_msec() / 1000.0
+	#})
+	pattern.Emit(ProjectilePattern.PatternType.CIRCLE, 25, {"power" : P})
 	
 # Override enemy death function to spawn minions after death
 func Death():
